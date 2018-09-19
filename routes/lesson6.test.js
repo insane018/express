0a82985b8ce37577e8ae9bfd6999462ea9ea46dd
zhangzeyu <zhangzy@wwwsan.com>
2018-09-19 16:28:36 +0800

    add routes

--- routes/lesson6.test.js
+++
@@ -1,7 +0,0 @@
-let main = require('./lesson6');
-let should = require('should');
-describe('test/main.test.js', function () {
-  it('should equal 55 when n === 10', function () {
-    main.fibonacci(10).should.equal(55);
-  });
-});
